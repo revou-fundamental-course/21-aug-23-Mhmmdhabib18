7e6f465fa0354cd802b406e605c477bd848421e7
Mhmmdhabib18 <mhmmdhabib987@gmail.com>
2024-02-06 07:45:48 -1200

    first commmit

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 var nama = prompt("Masukan nama anda");
 document.getElementById("nama").innerText = nama;
- if (nama == "") {
-    confirm ('Anda Tidak Mengisi Nama')
- } else {
-    alert ('Berhasil!!')
- }
+if (nama == "") {
+  confirm("Anda Tidak Mengisi Nama");
+} else {
+  alert("Berhasil!!");
+}
 
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-    showDivs((slideIndex += n));
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
-    var i;
-    var imgList = document.getElementsByClassName("img-slideshow");
-    if (n > imgList.length) slideIndex = 1;
-    else if (n < 1) slideIndex = imgList.length;
+  var i;
+  var imgList = document.getElementsByClassName("img-slideshow");
+  if (n > imgList.length) slideIndex = 1;
+  else if (n < 1) slideIndex = imgList.length;
 
-    for (i = 0; i < imgList.length; i++) {
-        imgList[i].style.display = "none";
-    }
+  for (i = 0; i < imgList.length; i++) {
+    imgList[i].style.display = "none";
+  }
 
-    imgList[slideIndex - 1].style.display = "block";
+  imgList[slideIndex - 1].style.display = "block";
 }
 
 setInterval(() => {
@@ -31,20 +31,19 @@ setInterval(() => {
 }, 5000);
 
 function validateForm() {
-    var name = document.forms["message-form"]["full-name"].value;
-    var date = document.forms["message-form"]["birth-date"].value;
-    var gender = document.forms["message-form"]["gender"].value;
-    var messages = document.forms["message-form"]["messages"].value;
+  var name = document.forms["message-form"]["full-name"].value;
+  var date = document.forms["message-form"]["birth-date"].value;
+  var gender = document.forms["message-form"]["gender"].value;
+  var messages = document.forms["message-form"]["messages"].value;
 
-    if (name == "" || date == "" || gender == "" || messages == "") {
-        alert("Harus Mengisi Semua Colom");
-        return false;
-    }
-
-    document.getElementById("sender-full-name").innerText = name;
-    document.getElementById("sender-birth-date").innerText = date;
-    document.getElementById("sender-gender").innerText = gender;
-    document.getElementById("sender-messages").innerText = messages;
+  if (name == "" || date == "" || gender == "" || messages == "") {
+    alert("Harus Mengisi Semua Colom");
     return false;
-}
+  }
 
+  document.getElementById("sender-full-name").innerText = name;
+  document.getElementById("sender-birth-date").innerText = date;
+  document.getElementById("sender-gender").innerText = gender;
+  document.getElementById("sender-messages").innerText = messages;
+  return false;
+}
